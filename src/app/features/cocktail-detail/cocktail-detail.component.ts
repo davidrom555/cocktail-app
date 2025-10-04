fcf215ca24b40f4b928cedfbf7e5819da085329d
david <davidrom_desarrollo@hotmai.com>
2025-10-04 18:27:00 -0300

    elimina variable que no se usa

--- src/app/features/cocktail-detail/cocktail-detail.component.ts
+++ src/app/features/cocktail-detail/cocktail-detail.component.ts
@@ -44,8 +44,6 @@ export class CocktailDetailComponent implements OnInit {
   showCategorySlider: boolean = false;
   categoryCocktails: Cocktail[] = [];
 
-  private previousCocktailId: string = '';
-
   constructor(
     private route: ActivatedRoute,
     private router: Router,
